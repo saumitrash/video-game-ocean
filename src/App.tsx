60ba53158df920fd690db8b5f780c86f5f42d515
darkyed <darkyed7@gmail.com>
2023-08-30 02:49:08 +0530

    created a basic navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, // >= 1024px
+      }}
+    >
+      <GridItem area={"nav"}>
+        <NavBar />
+      </GridItem>
+      <Show above="lg">
+        <GridItem area={"aside"} bg="royalblue">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area={"main"} bg="orange">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
