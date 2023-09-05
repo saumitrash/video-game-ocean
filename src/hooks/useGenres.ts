c31a6ea8e47650c33df766f9281a4d1bc3d13509
darkyed <darkyed7@gmail.com>
2023-09-06 05:24:32 +0530

    Load genres statically by shipping it with app

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,12 +1,14 @@
-import useData from "./useData";
+// import useData from "./useData";
+import genres from "../data/genres";
 
-export interface Genre{
-    id: number,
-    name: string,
-    image_background: string,
+export interface Genre {
+  id: number;
+  name: string;
+  image_background: string;
 }
 
-
-const useGenres = () => useData<Genre>('/genres');
+// const useGenres = () => useData<Genre>('/genres');
+// Same can be done for Platforms and SortOrder components
+const useGenres = () => ({ data: genres, isLoading: false, error: false });
 
 export default useGenres;
