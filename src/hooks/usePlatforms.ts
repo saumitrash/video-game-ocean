0ad6a1cb62adf1c5670d4e98a75b119235fa0b49
darkyed <darkyed7@gmail.com>
2023-09-28 12:44:37 +0530

    refactor: entity default interface

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { platforms } from "../data/platforms";
 import APIClient from "../services/api-client";
-import { Platform } from "../entities/Platform";
+import Platform from "../entities/Platform";
 import ms from "ms";
 
 const apiClient = new APIClient<Platform>("/platforms/lists/parents");
